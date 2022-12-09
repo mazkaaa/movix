9b89a22227edc87651b96160da3ba7851aacc66a
Muhammad Azka <hello.mazka@gmail.com>
2022-12-10 02:01:48 +0700

    Fixed interface

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,7 +6,42 @@ import Layout from "../components/layout";
 import Constant from "../components/constants";
 import MovieCard from "../components/reusables/movieCard";
 import { InferGetServerSidePropsType } from "next";
-import { MoviesInterface, SeriesInterface } from "./index.interface";
+
+interface MoviesInterface {
+  adult: boolean;
+  backdrop_path: string;
+  genre_ids: number[];
+  id: number;
+  media_type: string;
+  original_language: string;
+  original_title: string;
+  overview: string;
+  popularity: number;
+  poster_path: string;
+  release_date: string;
+  title: string;
+  video: boolean;
+  vote_average: DoubleRange;
+  vote_count: number;
+};
+
+interface SeriesInterface {
+  adult: boolean;
+  backdrop_path: string;
+  first_air_date: string;
+  genre_ids: number[];
+  id: number;
+  media_type: string;
+  name: string;
+  origin_country: string[];
+  original_language: string;
+  original_name: string;
+  overview: string;
+  popularity: number;
+  poster_path: string;
+  vote_average: DoubleRange;
+  vote_count: number;
+};
 
 export default function Home({ data }: InferGetServerSidePropsType<typeof getServerSideProps>) {
 
